73d46e0d8167c5f52e3cdf0208fcad3e7d1d45a9
liang xiao <indulgelx@gmail.com>
2024-03-29 00:09:49 +0800

    Update

---
+++ CS3305 Web programming 2/TiktokInterview.java
@@ -0,0 +1,47 @@
+
+
+// pageing  time stamp 
+// 
+pre-flight 1000 
+
+10 
+
+Paramter - size 10 20 30 // Requery Body { query : User name : .. }  size 20 
+
+request page 8 
+
+Return - { 
+    Query result {
+        key: value
+        obj :{ 
+            key : value 
+        } 
+    } : timestamp / Error:{} Httprespoonse Code : {time out authentication}
+    }
+
+    drop down list 20 
+    next page -> 
+
+    large file  10 B record - > remove duplciate 
+    1 row = 1 number 
+    2 G RAM 
+    int - in file txt file 
+    space 
+
+    16 4byte no space 
+    Set add  1B/2G size = remove dupldiate 
+    point 2G + 
+    Set 
+
+    4 SET 
+    final set 
+
+    compare set1 set 2 if hit set 2 in set1 remove set2 number 
+    new set 1+2 -> set 3 => set 1+2 +3 ->4 
+
+    
+    
+
+
+
+
